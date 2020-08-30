2065ba652151b711ef339dd2a38eaa9f00652fc1
Evan Heisler <evanheisler@gmail.com>
2020-08-29 23:23:43 -0500

    [Backend] Home page, page models

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,42 @@
+import { useState } from "react";
 import Head from "next/head";
 
-export default function Home() {
+const Home = ({ headline, personalAttributes }) => {
+  const [attr, setAttr] = useState(personalAttributes[0].personalAttribute);
+  const segments = headline.split(" ");
+
   return (
     <div>
       <Head>
-        <title>Evan Heisler :: (page) </title>
+        <title>Evan Heisler is a software engineer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>Let's gooooooooooo!</main>
+      <main>
+        <div>{segments[0]}</div>
+        <div>{segments[1]}</div>
+        <div>
+          {segments[2]} {attr}
+        </div>
+        <div>
+          {personalAttributes.map(({ id, personalAttribute }) => (
+            <span key={id} onClick={() => setAttr(personalAttribute)}>
+              -
+            </span>
+          ))}
+        </div>
+      </main>
     </div>
   );
-}
+};
+
+export const getStaticProps = async (ctx) => {
+  const res = await fetch(`${process.env.STRAPI_API_URL}/home-page`);
+  const props = await res.json();
+
+  return {
+    props,
+  };
+};
+
+export default Home;
